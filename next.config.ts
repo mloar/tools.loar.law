ef9dd59ed093d9db551dcac38a56268c65329a3f
Matt Loar <matt@loar.name>
2026-07-25 17:25:13 -0500

    Set basePath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['test.internal.loar.network'],
+  basePath: '/cook-county-order-builder',
 };
 
 export default nextConfig;
