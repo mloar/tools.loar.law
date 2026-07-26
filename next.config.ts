f5c695ef5742a186755a9a8ea704269a5518c810
Matt Loar <matt@loar.name>
2026-07-25 20:07:28 -0500

    Use static manifest

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['test.internal.loar.network'],
+  output: 'export',
 };
 
 export default nextConfig;
