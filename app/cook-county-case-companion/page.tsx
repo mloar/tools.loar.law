81ceb4a303ee8a37bcc8b1df86823c8cdbae0f3d
Matt Loar <matt@loar.name>
2026-08-15 16:11:54 -0500

    Add privacy policy for extension

---
+++ app/cook-county-case-companion/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <a href="privacy">Privacy Policy</a>
+    </div>
+  );
+}
---
+++ app/cook-county-case-companion/privacy/page.tsx
@@ -0,0 +1,23 @@
+import "./page.css"
+
+export default function Home() {
+  return (
+    <main>
+      <em>Effective Date: August 15, 2026</em>
+      <h1>Privacy Policy</h1>
+      <h2>Data Controller</h2>
+      <p>Matthew Edward Loar d/b/a Loar Law provides the Cook County Case Companion extension and controls how your personal data is used.</p>
+      <h2>Data Collected</h2>
+      <p>The following data is collected when using the Cook County Case Companion extension:</p>
+      <ul>
+        <li>Data available through the Cook County Case Portal ("Portal"), including party names</li>
+        <li>Document images opened in the Cook County Case Portal</li>
+        <li>Information you enter about yourself in the Prepared By block of the order builder</li>
+      </ul>
+      <h2>Purpose and Use</h2>
+      <p>Your data is collected to allow you to retrieve it while offline or not logged in to the Portal, and to draft orders in the order builder. Data collected never leaves your device, and is not sold or used for any other purpose.</p>
+      <h2>Contact Us</h2>
+      <p>If you have questions about this policy, you can contact us at <a href="mailto:help@loar.law">help@loar.law</a>.</p>
+    </main>
+  );
+}
